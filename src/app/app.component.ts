1a492542e2ea698c40f35ab4b9da89af10673522
DEZINA\dezina <dezinap.brainerhub@gmail.com>
2024-01-06 21:42:27 +0530

    changes in routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IndexComponent } from "./post/index/index.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, GoogleMapsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, GoogleMapsModule, IndexComponent]
 })
 export class AppComponent {
   title = 'my-angular-app';
